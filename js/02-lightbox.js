2eb776e733e1a4cd070204a19be14b9bfa6b2234
Alla <urt2509@gmail.com>
2023-04-12 12:31:25 +0200

    updated 02-gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+/**
+ * Function creates HTML for gallery of images
+ */
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+);
+
+/**
+ * Function insert created markup into HTML
+ */
+galleryList.insertAdjacentHTML("beforeend", markup.join(""));
+
+/**
+ * Create an instance for image lightbox
+ */
+const galleryListLightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionType: Attr,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
